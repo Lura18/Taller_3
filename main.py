07a128250501b0967e68cf9e35bb1c245083f23b
Laura Sarmiento <l.sarmientog@uniandes.edu.co>
2026-05-11 13:17:34 -0500

    Taller_3_FBD

---
+++ main.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pymongo import MongoClient
+from datetime import datetime
+import os
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+#os.environ para despliegue. Descomente cuando ya probó todo local.
+#client = MongoClient(os.environ["MONGO_URI"])
+client = MongoClient("mongodb://ISIS2304E28202610:KUYeUnb7wEgC@157.253.236.88:8087")
+
+db = client["ISIS2304E28202610"]
+
+
+@app.get("/")
+def inicio():
+    return {"estado": "API funcionando correctamente"}
+
+@app.get('/bares/{bar_id}/comentarios')
+def get_comentarios(bar_id: int):
+    comentarios = list(db["comentarios_bares"].find({'bar_id': bar_id},{'_id': 0}))
+    return comentarios
+
+@app.post('/bares/{bar_id}/comentarios')
+def post_comentario(bar_id: int, datos: dict):
+    datos['bar_id'] = bar_id
+    datos['fecha']  = datetime.now().isoformat()
+    # TODO: completar
+    return {'mensaje': 'Comentario guardado'}
+
+# TODO: implementar GET /bares/{bar_id}/eventos
+# Debe retornar todos los eventos del bar desde la colección 'eventos'
+
+# TODO: implementar POST /bares/{bar_id}/eventos  
+# Debe insertar el evento en la colección 'eventos'
+# Recuerde agregar bar_id y fecha_creacion al documento antes de insertar
